e38429fc80e88919e174c819111069a1e4971a68
Praadheep raj <152789092+Pradheepraj2K4@users.noreply.github.com>
2025-06-28 21:53:57 +0530

    animations added

--- src/Components/DecoLabs/Landing.jsx
+++ src/Components/DecoLabs/Landing.jsx
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { HalfLogo } from "../../assets";
 import { solutionImg1 } from "../../data";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Landing = () => {
+  const [hovering, setHovering] = useState("");
+
+  useGSAP(() => {
+    gsap.fromTo('#image', {
+      y: 100,
+      rotateZ: -8,
+      opacity: 0,
+      duration: 0.1
+    }, {
+      y: 0,
+      rotateZ: -3,
+      opacity: 1
+    })
+  }, [hovering])
+
+  useGSAP(() => {
+    gsap.to('#text-content', {
+      opacity: 0.3,
+      rotateZ: 4,
+      transformOrigin: '100% 50%', // right center
+      scrollTrigger: {
+        trigger: '.text-content-head',
+        start: 'top top',
+        end: 'bottom top',
+        scrub: true,
+      }
+    })
+  }, [])
   return (
     <div className="min-h-screen text-white bg-primaryBlack flex justify-between  flex-col relative">
-      <div className="absolute w-1/2 bottom-40 right-20 rotate-[15deg]">
-        <img
-          src={solutionImg1}
-          alt="Solution"
-          className="w-full h-full object-cover transition duration-500 z-50 hidden"
-        />
-      </div>
-      <div className="absolute flex flex-col justify-center bg-white items-center gap-5 text-black p-4 left-0 top-[50px] hidden">
+      <div className="absolute flex flex-col justify-center bg-white items-center gap-5 text-black p-4 left-0 top-[50px]">
         <img src={HalfLogo} alt="" className="w-8 invert" />
         <p
           className="text-lg font-bold font-garet"
@@ -24,18 +47,35 @@ const Landing = () => {
         </p>
       </div>
       <div className="max-md:hidde"></div>
-      <div className="mx-40 max-md:mx-6">
-        <h1 className="stroke-text text-[60px] font-extrabold leading-[100px] font-ilisarniq max-md:text-xl">
-          LARGE LANGUAGE MODELS: BEYOND THE PROMPT
-        </h1>
-        <h1 className="pt-8 text-xl font-medium font-lato max-md:text-sm">
-          <span className="underline">Cohort-based</span> Agentic AI{" "}
-          <span className="underline">bootcamp</span> to build practical
+      <div id="text-content" className="mx-40 max-md:mx-6 text-content-head">
+        <div className="relative">
+          {/* Hovering images */}
+          {
+            hovering !== "" && (
+              <div id="image" className="absolute top-0 z-5 -right-40 flex items-center justify-center">
+                <img
+                  src={solutionImg1}
+                  alt="Solution"
+                  className="w-[800px] aspect-[5/3] object-cover transition duration-500 z-50"
+                />
+              </div>
+            )
+          }
+          <h1 className="stroke-text text-[60px] font-extrabold leading-[100px] font-ilisarniq max-md:text-xl">
+            LARGE LANGUAGE MODELS: BEYOND THE PROMPT
+          </h1>
+          <h1 className="absolute inset-0 z-10 custom-stroke text-[60px] font-extrabold leading-[100px] font-ilisarniq max-md:text-xl">
+            LARGE LANGUAGE MODELS: BEYOND THE PROMPT
+          </h1>
+
+        </div>
+        <h1 className="pt-8 text-xl font-medium font-lato max-md:text-sm z-10">
+          <span className="underline" onMouseEnter={() => setHovering("Cohort-based")} onMouseLeave={() => setHovering("")}>Cohort-based</span> Agentic AI{" "}
+          <span className="underline" onMouseEnter={() => setHovering("bootcamp")} onMouseLeave={() => setHovering("")}>bootcamp</span> to build practical
           LLM-powered agents
         </h1>
-      </div>
       {/*<span className="text-white">scroll</span> */}
-      <div className="md:px-40 max-md:mx-6 md:w-[80%] pb-20 font-garet text-[#808080] max-md:text-xs text-justify text-[12.36px]">
+      <div className="max-md:mx-6 md:w-[80%] mt-20 pb-20 font-garet text-[#808080] max-md:text-xs text-justify text-[12.36px]">
         <p>
           This immersive{" "}
           <span className="text-white">
@@ -52,6 +92,11 @@ const Landing = () => {
           <span className="font-lato">-</span>world environments through hands
           <span className="font-lato">-</span>on projects and practical tools
         </p>
+      </div>
+      </div>
+
+      <div>
+
       </div>
     </div>
   );
